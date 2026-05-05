041b8d2c0089e72058bad74961b4ac9b09a5015c
Mohammed Sinan M.P <mosdotnet@gmail.com>
2026-05-05 22:33:00 +0530

    Some changes in android.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.syn.controlx"
+    namespace = "com.example.controlx"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.syn.controlx"
+        applicationId = "com.example.controlx"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
